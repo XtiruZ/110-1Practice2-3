366995c3a6cfb3350b1c444cd75631cf3f67eb9e
Xtiru <ghostking1018@gmail.com>
2021-12-10 11:40:49 +0800

    added Nar and Reflect aspx

---
+++ 110-1Practice2-3/Nar.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace _110_1Practice2_3
+{
+    public partial class Nar : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i_number, n, o_number1, o_number2, Squares1, Squares2, sum, i, j;
+            i_number = 153;
+            n = 0;
+            o_number1 = i_number;
+            o_number2 = i_number;
+            sum = 0;
+            while (o_number1 != 0)
+            {
+                o_number1 /= 10;
+                n++;
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                Squares1 = o_number2 % 10;
+                o_number2 /= 10;
+                Squares2 = Squares1;
+                for (j = 1; j < n; j++)
+                {
+                    Squares1 = Squares2 * Squares1;
+                }
+                sum += Squares1;
+            }
+
+            if (sum == i_number)
+            {
+                Response.Write("True");
+            }
+            else
+            {
+                Response.Write("False");
+            }
+
+
+
+        }
+    }
+}
---
+++ 110-1Practice2-3/Reflect.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _110_1Practice2_3
+{
+    public partial class Reflect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int M, r, sum, o_M, n;
+
+            M = 663166;
+            o_M = M;
+            sum = 0;
+            n = 0;
+            while (M > 0)
+            {
+                r = M % 10;
+                sum = (sum * 10) + r;
+                M = M / 10;
+                n++;
+            }
+
+            if (o_M == sum & n <= 8)
+            {
+                Response.Write("True");
+            }
+            else if (n > 8)
+            {
+                Response.Write("M值不能大於8"); ;
+            }
+            else
+            {
+                Response.Write("False");
+            }
+        }
+    }
+}
